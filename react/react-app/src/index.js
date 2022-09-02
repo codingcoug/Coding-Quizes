c8380518597c9f843f7a15314a02c30c2a71fe47
seth <sethkoywhiting@gmail.com>
2022-09-02 07:20:48 -0600

    added counter-app

--- react/react-app/src/index.js
+++ react/react-app/src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const element = <h1>Hello World!</h1>;
+//console.log(element);
+ReactDOM.render(element, document.getElementById('root'));
