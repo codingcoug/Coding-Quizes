92f6070031c3ba879a258cc58315bc3428160dd5
codingcoug <sethkoywhiting@gmail.com>
2022-09-11 13:30:36 -0600

    renamed counter

--- react/counter-app/src/components/counter.jsx
+++ react/counter-app/src/components/counter.jsx
@@ -12,23 +12,13 @@ class Counter extends Component {
 
         return (
         <div>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
+
             <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
         </div>
         );
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 bg-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
 
-    formatCount() {
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count;
-    }
 }
  
 export default Counter;
