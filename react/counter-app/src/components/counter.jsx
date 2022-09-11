1abf2665c6d2dd9eeab9f86ec0824d8dcda14d78
codingcoug <sethkoywhiting@gmail.com>
2022-09-11 00:13:20 -0600

    added list items

--- react/counter-app/src/components/counter.jsx
+++ react/counter-app/src/components/counter.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Counter extends Component { 
     state = {
         count: 0,
-        imageUrl: 'https://picsum.photos/200'
+        tags: ['tag1', 'tag2', 'tag3']
     };
     
+
     render() { 
+
         return (
         <div>
-            <img src={this.state.imageUrl} alt="" />
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className='btn btn-secondary btn-sm'>Increment</button>
+            <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
         </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 bg-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
